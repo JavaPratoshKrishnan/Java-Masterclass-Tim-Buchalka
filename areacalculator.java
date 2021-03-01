3e7fb448e96896fa6c7c0e5ad400aea91903262c
JavaPratoshKrishnan <79862107+JavaPratoshKrishnan@users.noreply.github.com>
2021-03-01 22:04:44 +0530

    Add files via upload

---
+++ areacalculator.java
@@ -0,0 +1,25 @@
+package Senthil.Java;
+
+public class areacalculator {
+    public static void main(String[] args) {
+        System.out.println("radius " + area(5.0));
+        System.out.println("rectangle" + area(5.0,4.0));
+
+    }
+
+    public static double area (double radius){
+        if (radius < 0){
+            return -1;
+        }
+        return radius*radius*Math.PI;
+
+    }
+    public static double area(double x, double y){
+        if ((x<0) || (y<0)){
+            return -1;
+        }
+
+        return x*y;
+    }
+
+}
