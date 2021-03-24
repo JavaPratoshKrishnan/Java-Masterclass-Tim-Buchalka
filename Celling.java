06d5616b68e78d68d9d990636d9780c8d1d8d331
JavaPratoshKrishnan <79862107+JavaPratoshKrishnan@users.noreply.github.com>
2021-03-24 18:01:23 +0530

    Add files via upload

---
+++ Celling.java
@@ -0,0 +1,19 @@
+package com.timbuchalka;
+
+public class Celling {
+    private int height;
+    private int paintedColor;
+
+    public Celling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
